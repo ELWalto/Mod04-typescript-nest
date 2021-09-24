8a8405ed719f6d3484afdc2c752f93044b2ef062
ElWalto <walterclean@gmail.com>
2021-09-23 23:05:15 -0300

    aula04

--- aula3-mod4/src/product/entities/product.entity.ts
+++ aula3-mod4/src/product/entities/product.entity.ts
@@ -1,1 +1,9 @@
-export class Product {}
+import { Prisma } from "@prisma/client";
+
+export class Product implements Prisma.ProductUncheckedCreateInput { // copia do q ta no banco
+    id?: number;
+    name: string;
+    price?: number;
+    images?: Prisma.ImageUncheckedCreateNestedManyWithoutProductInput;
+
+}
