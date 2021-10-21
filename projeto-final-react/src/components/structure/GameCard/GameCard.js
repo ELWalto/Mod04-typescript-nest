17cccb4f8a1cc7c19fab163031d5f1ef6663e5fd
Walter Carvalho <walterclean@gmail.com>
2021-10-20 21:19:00 -0300

    Subindo alterações backend pasta

--- projeto-final-react/src/components/structure/GameCard/GameCard.js
+++ projeto-final-react/src/components/structure/GameCard/GameCard.js
@@ -4,25 +4,36 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import { styled } from "@mui/system";
 
 const GameCard = (props) => {
-    return props.games.map((game, idx) => (
-        <Card sx={{ width: 160, marginTop: "3rem",padding:"0.3rem"}} key={`game-card${idx}`} className="game-card">
-            <CardMedia component="img" image={game.cover} height="280" className="game-card__image" />
-            <CardContentNoPadding>
-{/*                 <Typography gutterBottom variant="h6" component="div">
-                    {game.title}
-                </Typography> */}
-                <CardActionsEdited className="buttons-group">
-                    <Button size="small" variant="outlined">Details</Button>
-                    <Button size="small" variant="outlined">Favorite</Button>
-                </CardActionsEdited>
-            </CardContentNoPadding>
-        </Card>
-    ));
+  return props.games.map((game, idx) => (
+    <Card
+      sx={{ width: 180, marginTop: "2rem", padding: "0.2rem" }}
+      key={`game-card${idx}`}
+      className="game-card"
+    >
+      <CardMedia
+        component="img"
+        image={game.cover}
+        height="360"
+        className="game-card__image"
+      />
+      <CardContentNoPadding>
+        <CardActionsEdited className="buttons-group">
+          <Button size="large" variant="outlined">
+            {" "}
+            Details
+          </Button>
+          <Button size="large" variant="contained">
+            Favorite
+          </Button>
+        </CardActionsEdited>
+      </CardContentNoPadding>
+    </Card>
+  ));
 };
 
 const CardContentNoPadding = styled(CardContent)(`
@@ -30,13 +41,12 @@ const CardContentNoPadding = styled(CardContent)(`
     &:last-child {
         padding-bottom: 0
     }
-`)
+`);
 
 const CardActionsEdited = styled(CardActions)(`
     >:not(:first-of-type) {
         margin-left: 0
     }
-`)
-
+`);
 
 export default GameCard;
